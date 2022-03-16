b0aef7a5e6050b8e81259513955f2d38ac10622e
MicheleSavii <michelesavi987@gmail.com>
2022-03-16 09:28:41 +0100

    (8) aggiungere attributi privati di Articolo

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -9,7 +9,9 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
     class Articolo
     {
         // TODO: (8) aggiungere attributi privati di Articolo
-        // ...
+        public string Descrizione { set; get; }
+        public string UnitaMisura { set; get; }
+        public string Prezzo { set; get; }
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
         // ...
